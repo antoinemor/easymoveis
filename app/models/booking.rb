6eb96bd059c32714179a1b0636401604865422d9
Luis Felipe Cortes <lfe.cortes@gmail.com>
2016-12-06 16:54:20 -0200

    validations of booking model dates

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,20 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :listing
   has_one :furniture, through: :listing
+
+  validate :start_date_before_end_date
+  validate :start_date_before_past
+
+  private
+   def start_date_before_end_date
+     if self.start_date > self.end_date
+       errors.add(:start_date, "Start date must be before end date")
+     end
+   end
+
+   def start_date_before_past
+     if self.start_date < Date.today
+       errors.add(:start_date, "Start date must be since today")
+     end
+   end
 end
