2ef4501d9cfb7b6f7e6c0d36a6c6b85687efc2b0
Antoine Moreau <antoine_moreau@me.com>
2016-12-06 15:18:18 -0200

    Update furniture model

--- app/models/furniture.rb
+++ app/models/furniture.rb
@@ -1,6 +1,7 @@
 class Furniture < ApplicationRecord
-  # has_one :listing
-  #has_many :bookings, through: :listings
+  belongs_to :user
+  has_one :listing
+  has_many :bookings, through: :listings
 
   validates :name,        presence: true
   validates :description, presence: true
