51efbab2f5c7e1c25396447cef16e09df15e8b08
Antoine <antoine_moreau@me.com>
2016-12-06 10:48:22 -0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  get 'listings/index'
-
-  get 'listings/new'
-
-  get 'listings/edit'
-
-  get 'listings/show'
-
-  get 'listings/update'
-
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
