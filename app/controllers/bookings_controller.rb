ec5317941e3ac8edab2a73cf44675573330fc60f
Luis Felipe Cortes <lfe.cortes@gmail.com>
2016-12-07 17:08:55 -0200

    adding basic booking controller methods policies

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,7 +4,8 @@ class BookingsController < ApplicationController
   before_action :find_listing, only: [:new, :create, :index, :update, :destroy]
 
   def index
-    @bookings = @listing.bookings
+    #@bookings = @listing.bookings
+    @bookings = policy_scope(@listing.bookings)
   end
 
   def show
@@ -12,12 +13,14 @@ class BookingsController < ApplicationController
 
   def new
     @booking = @listing.bookings.new
+    authorize @booking
   end
-  
+
    def create
     @booking = @listing.bookings.new(booking_params)
     @booking.status = "E"
     @booking.user = current_user
+    authorize @booking
     if @booking.save
       redirect_to user_booking_list_path(@booking.id, @booking.listing), notice: 'Booking was successfully created.'
     else
@@ -34,7 +37,7 @@ class BookingsController < ApplicationController
     redirect_to user_booking_list_path(@booking.id, @booking.listing_id), notice: 'Booking was successfully updated.'
   end
 
-  def cancel_booking
+  def cancel_booking #by booking user_constumer
     @booking.update_attributes(status: "C")
     redirect_to user_booking_list_path(@booking.id, @booking.listing_id), notice: 'Booking was successfully cancelated.'
   end
@@ -44,18 +47,20 @@ class BookingsController < ApplicationController
       redirect_to bookings_url, notice: 'Booking was successfully destroyed.'
   end
 
-  def list_users
+  def list_users #TBD #by booking user_constumer
     @user = User.find(current_user)
     @bookings = @user.bookings
+    authorize @bookings
   end
 
-  def approve_booking
+  def approve_booking #by listing user_constumer
     @booking.status = "A"
     @booking.save
+    authorize @booking
     redirect_to listing_bookings_path(@booking.listing_id), notice: 'Booking accepted.'
   end
 
-  def reject_booking
+  def reject_booking #by listing user_constumer
     if @booking.status == "A"
       @booking.listing.vacancies += 1
       @listing.save
@@ -75,11 +80,12 @@ class BookingsController < ApplicationController
   def get_listing
     @listing = @booking.listing
   end
-  
+
   def find_booking
     @booking = Booking.find(params[:id])
+    authorize @booking
   end
-  
+
   def booking_params
     params.require(:booking).permit(:check_in, :check_out, :status, :user_id, :listing_id)
   end
