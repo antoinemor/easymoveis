86c89c9b16853423056a51b471bc7e6d1637ab9c
Antoine Moreau <antoine_moreau@me.com>
2016-12-07 10:08:42 -0200

    Add facebook connect

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def avatar_image(user)
+    if user.facebook_picture_url
+      return user.facebook_picture_url
+    elsif user.photo
+      return user.photo.path
+    else
+      return "http://placehold.it/150x150"
+    end
+  end
 end
