7cc84a051ee912ca121a62038f42fafe2b3e673c
Antoine <antoine_moreau@me.com>
2016-12-07 11:31:00 -0200

    Refactor bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,41 +5,33 @@ class BookingsController < ApplicationController
 
   def index
     @bookings = @listing.bookings
-
   end
 
   def show
   end
 
-  # GET /bookings/new
   def new
     @booking = @listing.bookings.new
   end
-
-  # GET /bookings/1/edit
-  def edit
-  end
-
-  # POST /bookings
-  def create
+  
+   def create
     @booking = @listing.bookings.new(booking_params)
     @booking.status = "E"
     @booking.user = current_user
-
     if @booking.save
       redirect_to user_booking_list_path(@booking.id, @booking.listing), notice: 'Booking was successfully created.'
     else
       render :new
     end
-
   end
 
-  # PATCH/PUT /bookings/1
+  def edit
+  end
+
   def update
     @booking.status = "E"
     @booking.update(booking_params)
     redirect_to user_booking_list_path(@booking.id, @booking.listing_id), notice: 'Booking was successfully updated.'
-
   end
 
   def cancel_booking
@@ -47,7 +39,6 @@ class BookingsController < ApplicationController
     redirect_to user_booking_list_path(@booking.id, @booking.listing_id), notice: 'Booking was successfully cancelated.'
   end
 
-  # DELETE /bookings/1
   def destroy
     @booking.destroy
       redirect_to bookings_url, notice: 'Booking was successfully destroyed.'
@@ -61,7 +52,7 @@ class BookingsController < ApplicationController
   def approve_booking
     @booking.status = "A"
     @booking.save
-    redirect_to listing_bookings_path(@booking.listing_id), notice: 'Booking acceped.'
+    redirect_to listing_bookings_path(@booking.listing_id), notice: 'Booking accepted.'
   end
 
   def reject_booking
@@ -77,25 +68,19 @@ class BookingsController < ApplicationController
 
   private
 
-    def find_listing
-      @listing = listing.find(params[:listing_id])
+  def find_listing
+    @listing = listing.find(params[:listing_id])
+  end
 
-    end
-
-    def get_listing
-
-      @listing = @booking.listing
-
-    end
-
-    # Use callbacks to share common setup or constraints between actions.
-    def find_booking
-      @booking = Booking.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def booking_params
-      params.require(:booking).permit(:check_in, :check_out, :status, :user_id, :listing_id)
-    end
-
+  def get_listing
+    @listing = @booking.listing
+  end
+  
+  def find_booking
+    @booking = Booking.find(params[:id])
+  end
+  
+  def booking_params
+    params.require(:booking).permit(:check_in, :check_out, :status, :user_id, :listing_id)
+  end
 end
