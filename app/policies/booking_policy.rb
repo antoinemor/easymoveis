9f93effd1b3efd588821daa1c632833e706227d3
Luis Felipe Cortes <lfe.cortes@gmail.com>
2016-12-08 10:43:07 -0200

    Adding policy to booking methods

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -21,9 +21,30 @@ class BookingPolicy < ApplicationPolicy
     user_is_owner_or_admin?
   end
 
+  def cancel_booking?
+    user_is_owner_or_admin?
+  end
+
+  def list_users?
+    user_is_owner_or_admin?
+  end
+
+  def approve_booking?
+    user_is_offering?
+  end
+
+  def reject_booking?
+    user_is_offering?
+  end
+
+
   private
 
   def user_is_owner_or_admin?
     record.user == user || user.admin
+  end
+
+  def user_is_offering?
+    record.listing.user == user || user.admin
   end
 end
