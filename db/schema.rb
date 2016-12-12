efe42f37d09c33ebfcb34bcae8b65e122f5567e2
Rodrigo Birolli <rodrigo.birolli@gmail.com>
2016-12-12 10:47:39 -0200

    Problemas do schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209175614) do
+ActiveRecord::Schema.define(version: 20161209161631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,6 @@ ActiveRecord::Schema.define(version: 20161209175614) do
     t.integer  "listing_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float    "latitude"
-    t.float    "longitude"
     t.index ["listing_id"], name: "index_addresses_on_listing_id", using: :btree
   end
 
