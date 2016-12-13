326757198e7911ff9c32152c036e4e0d5b1e9756
Antoine Moreau <antoine_moreau@me.com>
2016-12-13 11:02:59 -0200

    Update message to user on show

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,4 +10,14 @@ class MessagesController < ApplicationController
     flash[:success] = "Message has been sent!"
     redirect_to conversation_path(conversation)
   end
+
+  def new_to
+    @user = User.find(params[:user_id])
+  end
+
+  # def create_to(user)
+  #   conversation = current_user.send_message(user, params[:message][:body], params[:message][:subject]).conversation
+  #   flash[:success] = "Message has been sent!"
+  #   redirect_to conversation_path(conversation)
+  # end
 end
